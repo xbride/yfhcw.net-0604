a4270e80a5a24d74b889b35fad105f7d48a03e3e
xbride <mail@yfhcw.net>
2017-06-30 23:12:17 +0800

    add confirm before delete files to cleardir.php

--- gallery/cleardir.php
+++ gallery/cleardir.php
@@ -1,11 +1,24 @@
+  
+ 
+<html>
+<body>
+
+
+
 <?php
-
-include "f_copydir.php";
-include "f_delfiles.php";
-
-copyDir("gallery-images", "gallery-images" . date('y-m-d-h-i-s',time()));
-deleteFile("gallery-images");
-
-echo "操作已完成。";
-
+	echo '<script>';
+	echo 'if(true!=confirm("确定要清空所有图片吗？")){';
+	echo 'window.close();';
+	echo '}';
+	echo '</script>';
+	
+	include "f_copydir.php";
+	include "f_delfiles.php";		
+	copyDir("gallery-images", "gallery-images" . date('y-m-d-h-i-s',time()));
+	deleteFile("gallery-images");		
+	echo "操作已完成。";
 ?>
+
+
+</body>
+</html>
